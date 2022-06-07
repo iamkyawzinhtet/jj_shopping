8046f59702453125ecbed83b407573d73dbe6b9d
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2022-06-07 23:41:15 +0630

    add product feature

--- admin/index.php
+++ admin/index.php
@@ -7,121 +7,40 @@
     header('Location: login.php');
   }
 
+  if(!empty($_GET['pageno'])) {
+    $pageno = $_GET['pageno'];
+  }else {
+    $pageno = 1;
+  }
+  $numOfRecs = 5;
+  $offset = ($pageno - 1) * $numOfRecs;
+
+  if(empty($_POST['search'])) {
+    $stmt = $pdo->prepare("SELECT * FROM products ORDER BY id DESC");
+    $stmt->execute();
+    $rawResult = $stmt->fetchAll();
+    $total_pages = ceil(count($rawResult) / $numOfRecs);
+
+    $stmt = $pdo->prepare("SELECT * FROM products ORDER BY id DESC LIMIT $offset,$numOfRecs");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+  }else {
+    $searchKey = $_POST['search'];
+    $stmt = $pdo->prepare("SELECT * FROM products WHERE name LIKE '%$searchKey%' ORDER BY id DESC");
+    $stmt->execute();
+    $rawResult = $stmt->fetchAll();
+    $total_pages = ceil(count($rawResult) / $numOfRecs);
+
+    $stmt = $pdo->prepare("SELECT * FROM products WHERE name LIKE '%$searchKey%' ORDER BY id DESC LIMIT $offset,$numOfRecs");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+  }
+
   // print "<pre>";
   // print_r($result);
 ?>
 
-<!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>JJ Shopping</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
-
-    <?php
-        $link = $_SERVER['PHP_SELF'];
-        $link_array = explode('/', $link);
-        $page = end($link_array);
-    ?>
-    
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form class="form-inline" method="POST" action="index.php">
-          <input name="_token" type="hidden" value="<?php echo $_SESSION['_token']; ?>">
-            <div class="input-group input-group-sm">
-              <input name="search" class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">JJ Shopping | Product</a>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item">
-            <a href="index.php" class="nav-link">
-              <i class="nav-icon fas fa-box"></i>
-              <p>
-                Product
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="category.php" class="nav-link">
-              <i class="nav-icon fas fa-list"></i>
-              <p>
-                Category
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="user.php" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
-              <p>
-                User
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+ <?php include('header.php'); ?>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -130,7 +49,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-4">
-            <a href="add.php" class="btn btn-success">New Item</a>
+            <a href="product_add.php" class="btn btn-success">New Product</a>
           </div>
           <div class="col-sm-8">
             <ol class="breadcrumb float-sm-right">
@@ -153,13 +72,38 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <thead>
                     <tr>
                       <th style="width: 10px">#</th>
-                      <th>Title</th>
-                      <th>Content</th>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Category</th>
+                      <th>In Stock</th>
+                      <th>Price</th>
                       <th style="width: 150px">Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                      
+                    <?php
+                      $i = 1;
+                      foreach ($result as $value) {
+                        $catStmt = $pdo->prepare("SELECT * FROM categories WHERE id=".$value['category_id']);
+                        $catStmt->execute();
+                        $catResult = $catStmt->fetchAll();
+                    ?>
+                        <tr>
+                          <td><?php echo $i ?></td>
+                          <td><?php echo escape($value['name']) ?></td>
+                          <td><?php echo escape($value['description']) ?></td>
+                          <td><?php echo escape($catResult[0]['name']) ?></td>
+                          <td><?php echo escape($value['quantity']) ?></td>
+                          <td><?php echo escape($value['price']) ?></td>
+                          <td>
+                            <a href="product_edit.php?id=<?php echo($value['id'])?>" class="btn btn-primary">Edit</a>
+                            <a href="product_delete.php?id=<?php echo($value['id'])?>" class="btn btn-danger">Delete</a>
+                          </td>
+                        </tr>
+                    <?php
+                      $i++;
+                      }
+                    ?>
                   </tbody>
                 </table>
               </div>
@@ -167,7 +111,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
               <div class="card-footer clearfix">
                 <ul class="pagination pagination-sm m-0 float-right">
-                  
+                  <li class="page-item">
+                    <a class="page-link" href="?pageno=1">First</a>
+                  </li>
+                  <li class="page-item <?php if($pageno<=1){echo 'disabled';} ?>">
+                    <a class="page-link" href="<?php if($pageno<=1){echo '#';}else{echo "?pageno=".($pageno-1);} ?>">Previous</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="#"><?php echo $pageno; ?></a>
+                  </li>
+                  <li class="page-item <?php if($pageno>=$total_pages){echo 'disabled';} ?>">
+                    <a class="page-link" href="<?php if($pageno>=$total_pages){echo '#';}else{echo "?pageno=".($pageno+1);} ?>">Next</a>
+                  </li>
+                  <li class="page-item">
+                    <a class="page-link" href="?pageno=<?php echo $total_pages ?>">Last</a>
+                  </li>
                 </ul>
               </div>
             </div>
---
+++ admin/product_add.php
@@ -0,0 +1,230 @@
+<?php
+  session_start();
+  require '../config/config.php';
+  require '../config/common.php';
+
+  if(empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
+    header('Location: login.php');
+  }
+
+  if($_POST) {
+    if(empty($_POST['name']) || empty($_POST['description']) || empty($_POST['category'])|| empty($_POST['quantity'])|| empty($_POST['price']) || empty($_FILES['image']['name'])) {
+
+      if(empty($_POST['name'])) {
+        $nameError = 'Name is required';
+      }
+
+      if(empty($_POST['description'])) {
+        $descError = 'Description is required';
+      }
+
+      if(empty($_POST['category'])) {
+        $catError = 'Category is required';
+      }
+
+      if(empty($_POST['quantity'])) {
+        $qtyError = 'Quantity is required';
+      }elseif(is_numeric($_POST['quantity']) != 1) {
+        $qtyError = 'Quantity must be Integer';
+      }
+
+      if(empty($_POST['price'])) {
+        $priceError = 'Price is required';
+      }elseif(is_numeric($_POST['price']) != 1) {
+        $priceError = 'Price must be Integer';
+      }
+      
+      if(empty($_FILES['image']['name'])) {
+        $imageError = 'Image is required';
+      }
+
+    }else { //validation success
+      $file = 'images/'.($_FILES['image']['name']);
+      $imageType = pathinfo($file,PATHINFO_EXTENSION);
+
+      if($imageType != 'jpg' && $imageType != 'jpeg' && $imageType != 'png') {
+          echo "<script>alert('Image must be jpg,jpeg or png');</script>";
+      }else {
+          $name = $_POST['name'];
+          $desc = $_POST['description'];
+          $category = $_POST['category'];
+          $quantity = $_POST['quantity'];
+          $price = $_POST['price'];
+          $image = $_FILES['image']['name'];
+
+          move_uploaded_file($_FILES['image']['tmp_name'],$file);
+          
+          $stmt = $pdo->prepare("INSERT INTO products (name,description,category_id,quantity,price,image) VALUES (:name,:descripiton,:category,:quantity,:price,:image)");
+
+          $result = $stmt->execute(
+              array(
+                  ':name'=>$name,':descripiton'=>$desc,':category'=>$category,':quantity'=>$quantity,':price'=>$price,':image'=>$image
+              )
+          );
+
+          if($result) {
+            echo "<script>alert('Product is added');window.location.href=('index.php');</script>";
+          }
+
+      }
+    }
+      // print "<pre>";
+      // print_r($result);
+  }
+
+?>
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Add Product</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Product</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Product
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-12">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+              <li class="breadcrumb-item active"><a href="logout.php">Logout</a></li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <div class="container-fluid pl-3 pr-3">
+        <form action="product_add.php" method="POST" enctype="multipart/form-data">
+        <input name="_token" type="hidden" value="<?php echo $_SESSION['_token']; ?>">
+        <div class="form-group mb-4">
+            <label for="name" class="form-label" style="color: #666">Name</label>
+            <p style="color: red"><?php echo empty($nameError) ? '' : '*'.$nameError; ?></p>
+            <input type="text" name="name" class="form-control">
+        </div>
+        <div class="form-group mb-4">
+            <label for="description" class="form-label" style="color: #666">Description</label>
+            <p style="color: red"><?php echo empty($descError) ? '' : '*'.$descError; ?></p>
+            <textarea name="description" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+
+        <?php 
+            $catStmt = $pdo->prepare("SELECT * FROM categories");
+            $catStmt->execute();
+            $catResult = $catStmt->fetchAll();
+        ?>
+
+        <div class="form-group mb-4">
+            <label for="category" class="form-label" style="color: #666">Category</label>
+            <p style="color: red"><?php echo empty($catError) ? '' : '*'.$catError; ?></p>
+            <select name="category" class="form-control">
+                <option value="">Select Category</option>
+                <?php foreach ($catResult as $value) { ?>
+                    <option value="<?php echo $value['id'] ?>"><?php echo $value['name'] ?></option>
+                <?php } ?> ?>
+            </select>
+        </div>
+        <div class="form-group mb-4">
+            <label for="quantity" class="form-label" style="color: #666">Quantity</label>
+            <p style="color: red"><?php echo empty($qtyError) ? '' : '*'.$qtyError; ?></p>
+            <input type="number" name="quantity" class="form-control">
+        </div>
+        <div class="form-group mb-4">
+            <label for="price" class="form-label" style="color: #666">Price</label>
+            <p style="color: red"><?php echo empty($priceError) ? '' : '*'.$priceError; ?></p>
+            <input type="number" name="price" class="form-control">
+        </div>
+        <div class="form-group mb-4">
+            <label for="image" class="form-label" style="color: #666">Image</label>
+            <p style="color: red"><?php echo empty($imageError) ? '' : '*'.$imageError; ?></p>
+            <input type="file" name="image">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-success mr-1">Add</button>
+        </div>
+        </form>
+    </div>
+    
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <!-- Main Footer -->
+  <footer class="main-footer" style="text-align: center;">
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="#">Blog App</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
